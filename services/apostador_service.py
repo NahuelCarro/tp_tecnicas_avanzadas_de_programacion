96346a1f906b66813fc9bf06d3a62497349e8669
Nahuel Carro <nahuelagustincarro@gmail.com>
2024-10-29 20:03:39 -0300

    Actualizo apostador service

--- services/apostador_service.py
+++ services/apostador_service.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import HTTPException
 from models.apostador import Apostador
 from repository.apostador_repository import ApostadorRepository
@@ -13,11 +14,16 @@ class ApostadorService:
     def crear_apostador(
         self, nombre: str, mail: str, clave: str, es_admin: bool
     ) -> Apostador:
-        if self.apostador_repository.get_by_mail(mail):
+        if self.apostador_repository.obtener_por_mail(mail):
             raise HTTPException(
                 status_code=400, detail="Ya existe un apostador con ese mail"
             )
 
-        apostador = Apostador(nombre=nombre, mail=mail, clave=clave, es_admin=es_admin)
-        self.apostador_repository.save(apostador)
+        apostador = Apostador(
+            nombre=nombre, mail=mail, clave=clave, es_admin=es_admin
+        )
+        self.apostador_repository.guardar(apostador)
         return apostador
+
+    def obtener_apostador(self, mail: str) -> Optional[Apostador]:
+        return self.apostador_repository.obtener_por_mail(mail)
