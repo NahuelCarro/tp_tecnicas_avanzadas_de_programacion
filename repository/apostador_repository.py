0111645698c21240b8cab7e3fef6e7f7ca0da2e2
Nahuel Carro <nahuelagustincarro@gmail.com>
2024-10-29 20:03:09 -0300

    Actualizo apostador repository

--- repository/apostador_repository.py
+++ repository/apostador_repository.py
@@ -1,18 +1,10 @@
 from typing import Optional
 from sqlmodel import select
 from models.apostador import Apostador
-from database import SessionDep
+from .base_repository import BaseRepository
 
 
-class ApostadorRepository:
-    def __init__(self, session: SessionDep):
-        self.session = session
-
-    def get_by_mail(self, mail: str) -> Optional[Apostador]:
+class ApostadorRepository(BaseRepository):
+    def obtener_por_mail(self, mail: str) -> Optional[Apostador]:
         statement = select(Apostador).where(Apostador.mail == mail)
         return self.session.exec(statement).first()
-    
-    def save(self, apostador: Apostador):
-        self.session.add(apostador)
-        self.session.commit()
-        self.session.refresh(apostador)
