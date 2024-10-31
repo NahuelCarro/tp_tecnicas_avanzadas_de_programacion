6e0323f4494958fe06581c55fa6d1fd3787e24b8
Nahuel Carro <nahuelagustincarro@gmail.com>
2024-10-31 00:22:45 -0300

    Actualizo el dto de caballo

--- schemas/caballo_dto.py
+++ schemas/caballo_dto.py
@@ -1,9 +1,13 @@
 from sqlmodel import SQLModel
-
+from pydantic import Field
 from models.caballo import Caballo
 
 
 class CaballoDTO(SQLModel):
+    nombre: str = Field(description="El nombre del caballo")
+    peso: float = Field(description="El peso del caballo")
+    cuota: float = Field(description="La cuota del caballo")
+
     def __init__(self, caballo: Caballo):
         self.nombre = caballo.nombre
         self.peso = caballo.peso
