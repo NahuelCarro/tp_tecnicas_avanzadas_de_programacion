93c0ec885fd1bcea4525aacba1ab1b35e28ac5d7
Nahuel Carro <nahuelagustincarro@gmail.com>
2024-11-06 22:14:02 -0300

    Actualizo el main.py

--- main.py
+++ main.py
@@ -18,25 +18,25 @@ def crear_caballos_iniciales():
     try:
         caballo_repo = CaballoRepository(session)
         caballo_service = CaballoService(caballo_repo)
-        
+
         caballos_iniciales = [
-            ("Relámpago", 450.5, 2.5),
-            ("Tormenta", 480.0, 3.0),
-            ("Fuego", 490.0, 2.7),
-            ("Trueno", 500.0, 2.3),
-            ("Ráfaga", 510.0, 2.1),
-            ("Viento", 520.0, 1.9),
+            ("Relámpago", 450.5),
+            ("Tormenta", 480.0),
+            ("Fuego", 490.0),
+            ("Trueno", 500.0),
+            ("Ráfaga", 510.0),
+            ("Viento", 520.0),
         ]
-        
-        for nombre, peso, cuota in caballos_iniciales:
+
+        for nombre, peso in caballos_iniciales:
             try:
                 print(f"Creando caballo {nombre}...")
-                caballo_service.crear_caballo(nombre, peso, cuota)
+                caballo_service.crear_caballo(nombre, peso)
                 print(f"Caballo {nombre} creado exitosamente.")
             except HTTPException:
                 print(f"El caballo {nombre} ya existe. Saltando...")
                 session.rollback()
-        
+
         session.commit()
     except Exception as e:
         print(f"Error al crear caballos iniciales: {e}")
