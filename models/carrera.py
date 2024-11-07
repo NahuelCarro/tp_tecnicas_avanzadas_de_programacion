bb0f67d91a416bef5a181b7f0d796a937cd38df1
Nahuel Carro <nahuelagustincarro@gmail.com>
2024-11-06 22:10:22 -0300

    Agrego metodos al modelo de carrera para pagar apuestas

--- models/carrera.py
+++ models/carrera.py
@@ -30,3 +30,39 @@ class Carrera(SQLModel, table=True):
         ganador = random.choice(self.caballos)
         self.caballo_ganador = ganador
         self.caballo_ganador_id = ganador.id
+        self.pagar_apuestas(self.apuestas)
+
+    def pagar_apuestas(self, apuestas: list["Apuesta"]):
+        for apuesta in apuestas:
+            if apuesta.caballo == self.caballo_ganador:
+                apuesta.apostador.balance_apuestas += (
+                    apuesta.monto * self.porcentaje_ganador(apuesta.caballo)
+                )
+            else:
+                apuesta.apostador.balance_apuestas -= apuesta.monto
+
+    def porcentaje_ganador(self, caballo: "Caballo") -> float:
+        """Calcula el porcentaje ganador para un caballo específico basado en
+        la cantidad de apuestas realizadas a él.
+
+        Args:
+            caballo (Caballo): Instancia del caballo para el cual calcular el porcentaje.
+
+        Raises:
+            ValueError: Error si no hay apuestas realizadas al caballo especificado.
+
+        Returns:
+            float: Porcentaje ganador como float.
+        """
+        cantidad_apuestas = sum(
+            1 for apuesta in self.apuestas if apuesta.caballo_id == caballo.id
+        )
+
+        if cantidad_apuestas == 0:
+            raise ValueError("No hay apuestas realizadas al caballo especificado.")
+
+        porcentaje = 2.00 - 0.10 * (cantidad_apuestas - 1)
+
+        porcentaje = max(porcentaje, 1.01)
+
+        return porcentaje
