78a4772e61d947e6c8f768965e8b1bfd0644841b
Nahuel Carro <nahuelagustincarro@gmail.com>
2024-11-09 11:54:07 -0300

    Actualizo las excepciones

--- exceptions.py
+++ exceptions.py
@@ -30,7 +30,7 @@ class UsuarioNoEncontradoException(HTTPException):
 class EmailOClaveIncorrectaException(HTTPException):
     def __init__(self):
         super().__init__(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Email o contraseña incorrectos",
             headers={"WWW-Authenticate": "Bearer"},
         )
@@ -87,7 +87,7 @@ class LimiteOffsetNegativoException(HTTPException):
 class UsuarioNoAdministradorException(HTTPException):
     def __init__(self, detail: str):
         super().__init__(
-            status_code=status.HTTP_401_UNAUTHORIZED,
+            status_code=status.HTTP_403_FORBIDDEN,
             detail=detail,
         )
 
@@ -120,7 +120,7 @@ class CarreraNoIniciadaException(HTTPException):
     def __init__(self):
         super().__init__(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="La carrera no ha comenzado",
+            detail="La carrera no puede iniciarse antes de la fecha programada",
         )
 
 
